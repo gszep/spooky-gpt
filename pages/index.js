73eef498f415ea4b809f87c3b267ca9e26f68d2b
Grisha Szep <gregory.szep@gmail.com>
2023-10-22 15:28:23 +0100

    strip down to basics

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
 
 function prompt() {
@@ -17,21 +16,6 @@ function prompt() {
 }
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
-
   return (
     <main className={styles.main}>
       <h1>The Bewitching Bot</h1>
